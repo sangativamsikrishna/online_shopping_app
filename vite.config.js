d754756440a5f0b2ce9423d131d0407343d1db5e
sangativamsikrishna <117709513+sangativamsikrishna@users.noreply.github.com>
2025-03-02 01:04:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: './',
-    css: {
-      devSourcemap: false
-    }
-})
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: 'all',  // Temporarily allow all hosts
+    cors: true  
+  }
+});
